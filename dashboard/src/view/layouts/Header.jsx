3dd5390684689f8c6685496783d281a638222644
Javad Esmaeili <cs.esmaeili@gmail.com>
2021-08-26 19:00:42 +0430

    header icons added

--- dashboard/src/view/layouts/Header.jsx
+++ dashboard/src/view/layouts/Header.jsx
@@ -1,7 +1,23 @@
+import { FaCog, FaRegBell, FaRegEnvelope, FaCalendarAlt } from "react-icons/fa";
 const Header = () => {
+
     return (
-        <div className='header'>
-            salam header
+        <div className='d-flex justify-content-start flex-row header'>
+            <div className='header-icon'>
+                <div className='header-item-badge'>9</div>
+                <FaCog />
+            </div>
+            <div className='header-icon'>
+                <div className='header-item-badge'>9</div>
+                <FaRegBell />
+            </div>
+            <div className='header-icon'>
+                <FaRegEnvelope />
+            </div>
+            <div className='header-icon'>
+                <div className='header-item-badge'>9</div>
+                <FaCalendarAlt />
+            </div>
         </div>
     );
 }
